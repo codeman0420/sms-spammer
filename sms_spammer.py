84e370937e42b2cfede06b98bebc227a8547a093
Codeman <143707684+codeman0420@users.noreply.github.com>
2023-10-28 17:17:47 +0700

    Update sms_spammer.py

--- sms_spammer.py
+++ sms_spammer.py
@@ -1,4 +1,4 @@
-import time, os, platform, random, requests, threading,getpass
+import time, os, platform, random, requests, threading
 from user_agent import generate_user_agent
 
 global cmd, headers, proxy
@@ -44,7 +44,7 @@ def main():
 
     print(logo)
 
-    phone = getpass.getpass(f"{B}[*] Phone number: ")
+    phone = input(f"{B}[*] Phone number: ")
     amt = int(input(B + "[*] Count: "))
 
     flush_text("="*28, G)
